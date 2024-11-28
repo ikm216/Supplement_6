ae11cf6b8a85b87a06212fd6dac66bc8c709362b
ikm216 <ikmclassof22@gmail.com>
2024-11-28 18:39:17 -0500

    test: Added test to get a specified shape array of normally distributed numbers with a given mean and standard deviation.

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-def test():
-    pass
+import numpy as np
 
-test
+def test_should_return_specified_shape_array():
+    shape = (10,10)
+    mean = 5
+    stan_dev = 2
+    shape_arr = shape_array(shape, mean, stan_dev)
+
+    assert shape_arr.shape == shape
+    assert abs(np.mean(shape_arr) - mean) < 0.5
+    assert abs(np.stan_dev(shape_arr) - stan_dev) < 0.5
+
