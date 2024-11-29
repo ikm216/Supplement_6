af9687065de9fcb80799c6f5163f615919c889c3
ikm216 <ikmclassof22@gmail.com>
2024-11-28 19:46:05 -0500

    test: Fixed an error on the test to get the answer using the cramers rule.

--- test.py
+++ test.py
@@ -14,6 +14,8 @@ def shape_array(shape, mean, stan_dev):
     """
     return np.full(shape, mean) + stan_dev #np.full: creates the shape array with the specified value
 
+
+
 def test_should_return_specified_shape_array():
     shape = (10,10)
     mean = 5
@@ -24,7 +26,7 @@ def test_should_return_specified_shape_array():
     assert np.all(shape_arr == mean + stan_dev) #np.all: tests all values in the array evaulate to true with a specified axis 
 
 def test_should_return_negative1_4_from_cramers_rule():
-    coe_matrix = np.array([5, 3], [3, -5])
+    coe_matrix = np.array([[5, 3], [3, -5]])
     conts = np.array([7, -23])
 
     correct_answer = np.array([-1, 4])
